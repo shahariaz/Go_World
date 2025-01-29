b2814e5ed20ba9a8f82d20514a28e63b7d02f6c6
shahariaz <shahariaz.info@gmail.com>
2025-01-29 17:09:41 +0600

    feat: create print function using generics and learn different way to use generics

---
+++ generics/generics.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printSlice[T any] (items []T) {
+
+	for _, item := range items {
+		fmt.Println(item)
+	}
+
+}
+func compPrint[T comparable,V string](items []T) {
+	for _, item := range items {
+		fmt.Println(item)
+	}
+}
+func printSliceIntString[T string | int](items []T){
+	for _, item := range items{
+		fmt.Println(item)
+	}
+}
+type Stack[T any] struct{
+	elements []T
+}
+func main() {
+	myStack := Stack[int] {
+		elements: []int{1,2,3,4,5},
+	}
+	fmt.Println(myStack.elements)
+  printSlice([]int{1,2,3,4,5})
+	printSliceIntString([]string{"Hello", "World", "!"})
+}
