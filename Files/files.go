40cda333d70f026069f2bf1df9fe4bd95bf88860
shahariaz <shahariaz.info@gmail.com>
2025-02-01 23:17:30 +0600

    Read,Write in file and make file

--- Files/files.go
+++ Files/files.go
@@ -3,14 +3,39 @@ package main
 import "os"
 
 func main() {
-	f,err:=os.Open("text.txt")
-	if err!=nil{
-    panic(err)
-  }
+ //create file
+ f,err := os.Create("example.txt")
+ if err != nil {
+	 panic(err)
+	  }
+defer f.Close()
 
-	fileInfo,err:=f.Stat()
-	if err!=nil{
-		panic(err)
-  }
-	println("file name: ",fileInfo.Name())
-}
+f.WriteString("hello world\n")
+
+
+	// f,err:=os.Open("text.txt")
+	// if err!=nil{
+  //   panic(err)
+  // }
+
+	// fileInfo,err:=f.Stat()
+	// if err!=nil{
+	// 	panic(err)
+  // }
+	// println("file name: ",fileInfo.Name())
+	
+	//Reading a file
+	//  f,err:=os.Open("text.txt")
+  //   if err!=nil{
+	// 		panic(err)
+	// 	}
+	// 	defer f.Close()
+	// 	buf:=make([]byte,1000)
+	// 	d,err := f.Read(buf)
+	// 	if err != nil{
+	// 		panic(err)
+	// 	}
+	// 	println("bytes read: ",d)
+	// 	println("buffer: ",buf)
+	// 	println("content: ",string(buf))
+	}
